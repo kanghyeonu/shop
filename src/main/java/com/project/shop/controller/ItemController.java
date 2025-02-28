9f10827664a0d68421834219b7eabba443a8a5df
KHW <51577531+kanghyeonu@users.noreply.github.com>
2025-02-28 17:18:42 +0900

    thymeleaf 추가
    - 동적 페이지 생성

--- src/main/java/com/project/shop/controller/ItemController.java
+++ src/main/java/com/project/shop/controller/ItemController.java
@@ -1,8 +1,15 @@
 package com.project.shop.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class ItemController {
 
+    @GetMapping("/list")
+    String getItemList(Model model){
+        model.addAttribute("name", "sample");
+        return "itemList.html";
+    }
 }
