f98acb92393ecb227a2c2963571dae29df02769c
KHW <51577531+kanghyeonu@users.noreply.github.com>
2025-03-05 17:19:13 +0900

    JPA 추가
    - 레포지토리 인터페이스 구현

--- src/main/java/com/project/shop/controller/ItemController.java
+++ src/main/java/com/project/shop/controller/ItemController.java
@@ -1,14 +1,31 @@
 package com.project.shop.controller;
 
+import com.project.shop.domain.Item;
+import com.project.shop.repository.ItemRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import java.util.List;
+
+
 @Controller
+@RequiredArgsConstructor
 public class ItemController {
+
+    private final ItemRepository itemRepository;
+
+//    Lombok 쓰면 필요없음
+//    @Autowired
+//    public ItemController(ItemRepository itemRepository) {
+//        this.itemRepository = itemRepository;
+//    }
 
     @GetMapping("/list")
     String getItemList(Model model){
+        List<Item> result = itemRepository.findAll();
         model.addAttribute("name", "sample");
         return "itemList.html";
     }
