3230e4373436ca8cd6c66d6c82851ee86015fba4
KHW <51577531+kanghyeonu@users.noreply.github.com>
2025-04-03 17:48:14 +0900

    주석 추가

--- src/main/java/com/project/shop/config/SecurityConfig.java
+++ src/main/java/com/project/shop/config/SecurityConfig.java
@@ -30,9 +30,14 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http.csrf((csrf) -> csrf.disable()); // csrf 끄기
+
+        // <input type="hidden" th:name="${_csrf.parameterName}" th:value="${_csrf.token}">
+        // csrf를 킨다면 form에 위의 코드를 삽입해줘서 서버가 발급한 token 정보도 서버에게 전달 해야함
+        // csrf를 키면 ajax에서도 토큰을 넣어줘야함 -> 검색 해보자
 //        http.csrf(csrf -> csrf.csrfTokenRepository(csrfTokenRepository())
 //                .ignoringRequestMatchers("/login") // 보안을 끌 페이지 설정 안하는게 좋음
 //        );
+
         http.authorizeHttpRequests((authorize) ->
                 authorize.requestMatchers("/**").permitAll() // permitAll 모든 유저의 접속을 허락
         );
